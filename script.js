cbdd9af93cbe43673cf88fab37735ff5e2f9fc3d
Kevon Johnson <kevonjohnson@kevons-mbp.mynetworksettings.com>
2023-06-07 18:28:17 -0400

    error handling applied to generate password

--- script.js
+++ script.js
@@ -1,120 +1,132 @@
 // Assignment code // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.',
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z',
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-var userchosen=[]
-var password=""
+var userchosen = [];
+var password = "";
 
-function generatePassword(){
-  var length=prompt("how long do you want the password to be minimum 8, maximum 128")
-  var special=confirm("click ok to include special characters")
-  var numbers=confirm("click ok to include numbers")
-  var uppercase=confirm("click ok to include uppercase")
-  var lowercase=confirm("click ok to include lowercase")
-  if(special==true){
-    userchosen=userchosen.concat(specialCharacters)
+function generatePassword() {
+  var length = parseInt(
+    prompt("how long do you want the password to be minimum 8, maximum 128"));
+  if (length < 8 || length > 128) {
+    alert("password cannot be less than 8 or greater than 128");
+    return "";
   }
-  if(numbers==true){
-   userchosen=userchosen.concat(numericCharacters) 
+  if(Number.isNaN(length)){
+    alert("length must be a number")
+    return""
   }
-  if(uppercase==true){
-    userchosen=userchosen.concat(upperCasedCharacters)
+  var special = confirm("click ok to include special characters");
+  var numbers = confirm("click ok to include numbers");
+  var uppercase = confirm("click ok to include uppercase");
+  var lowercase = confirm("click ok to include lowercase");
+  if (special == true) {
+    userchosen = userchosen.concat(specialCharacters);
   }
-  if(lowercase==true){
-    userchosen=userchosen.concat(lowerCasedCharacters)
+  if (numbers == true) {
+    userchosen = userchosen.concat(numericCharacters);
+  }
+  if (uppercase == true) {
+    userchosen = userchosen.concat(upperCasedCharacters);
+  }
+  if (lowercase == true) {
+    userchosen = userchosen.concat(lowerCasedCharacters);
+  }
+  if (!special && !numbers && !uppercase && !lowercase) {
+    alert(" you must choose at least 1 kind of character");
+    return "";
   }
   for (let i = 0; i < length; i++) {
-   password=password+userchosen[Math.floor(Math.random()*userchosen.length)] 
-    
+    password =
+      password + userchosen[Math.floor(Math.random() * userchosen.length)];
   }
-return password
+  return password;
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -125,7 +137,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
